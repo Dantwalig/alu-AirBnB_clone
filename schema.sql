2964d7133d65cbb9ff66ead7be131a506e114d9e
divinecoder-150 <d.mutesi1@alustudent.com>
2026-01-21 03:22:45 -0800

    Add complete Airbnb database schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE airbnb_clone;
+USE airbnb_clone;
+
+CREATE TABLE users (...);
+CREATE TABLE states (...);
+CREATE TABLE cities (...);
+CREATE TABLE places (...);
+CREATE TABLE amenities (...);
+CREATE TABLE place_amenity (...);
+CREATE TABLE reviews (...);
+
